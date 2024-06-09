648f1e40c3283b8b765266c8d16b531528e9e35e
Chamarthi Chandhini <95535625+chandhini4509@users.noreply.github.com>
2024-06-09 20:17:07 +0530

    msg

---
+++ Helloo.java
@@ -0,0 +1,7 @@
+package sunny;
+public class Helloo{
+    public static void main(String[] args) {
+        System.out.println("Hellooo....");
+    }
+    
+}
---
+++ Welcome.java
@@ -0,0 +1,7 @@
+package sunny;
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("wellcome.....");
+    }
+    
+}
