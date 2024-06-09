6faddb4c26f8947ce5202aea5cc3896dc4d2a939
Chamarthi Chandhini <95535625+chandhini4509@users.noreply.github.com>
2024-06-09 20:25:45 +0530

    Update Sanitha.java

--- Sanitha.java
+++ Sanitha.java
@@ -2,7 +2,7 @@ package sunny;
 
 public class Sanitha{
     public static void main(String[] args) {
-        System.out.println("it is sunday today");
+        System.out.println("it is sunday today....so we eat chicken todayy");
     }
     
 }
